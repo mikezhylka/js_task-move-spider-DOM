76b22c34087078de143672e24f1d6ef3c3f62675
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-20 14:58:07 +0200

    make build

---
+++ index.e864d8a2.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.addEventListener("click",function(c){var n=e.getBoundingClientRect();t.style.top="".concat(c.clientY-n.top-t.clientHeight/1.5,"px"),t.style.left="".concat(c.clientX-n.left-t.clientWidth/1.5,"px")});
+//# sourceMappingURL=index.e864d8a2.js.map
